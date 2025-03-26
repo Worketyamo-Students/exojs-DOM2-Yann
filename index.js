b173d02ca1504ae2640e8cd750982b636f82ba0d
Yvanov237 <ngongnouyann@gmail.com>
2025-03-26 13:32:34 +0100

    realisation des exercice sur le dom js terminer

---
+++ index.js
@@ -0,0 +1,41 @@
+let balise = document.querySelector("body");
+
+balise.style.textDecorationStyle = 'none'
+balise.style.background = "black"
+balise.style.color = "white"
+
+liste = document.createElement("ul");
+balise.appendChild(liste);
+
+for (let i=1; i <= 4; i++){
+    li = document.createElement("li");
+    li.textContent = `Liste ${i}`;
+    liste.appendChild(li);
+    li.className = "item" +i; 
+}
+if (liste.lastChild){
+    liste.removeChild(liste.lastChild)
+}
+
+let  p = prompt("Entrez votre phrase")
+const sec = p.split(",")
+console.log(sec)
+for (let i=0; i < sec.length; i++){
+    li = document.createElement("li");
+    li.textContent = sec[i]
+    liste.appendChild(li);
+    li.className = "item" +i; 
+}
+// for ( let i= 0 ; i< sec.length ; i++){
+//     let l = document.createElement("li")
+//     l.textContent = sec[i] 
+//     liste.appendChild(l)
+//     l.id = "item-" +i
+//     l.className = "item" +i
+
+// }
+document.body.appendChild(liste)
+
+console.log()
+
+console.log(balise)
